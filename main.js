5bc4073f7a6dc38100b43084e3f316fcde4abe7d
LEMZEN <LEMZEN@hotmail.com>
2020-01-08 17:21:20 +0200

    -

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-
 const insert = (num) => {
+
     document.form.textview.value += num;
+
 }
 
 const clean = () => {
+
     document.form.textview.value = '';
+
 }
 
 const back = () => {
+
     let exp = document.form.textview.value;
+
     document.form.textview.value = exp.substring(0, exp.length - 1);
+
 }
 
 const equal = () => {
+
     let exp = document.form.textview.value;
+
     if (exp && eval(exp) != 'Infinity') {
+
         document.form.textview.value = eval(exp);
+
     }
 
-
 }
+
